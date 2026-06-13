38cfe9b0fc26dca6ad4295ae911cfb37908fa5b2
zero2005x <zero2005x@gmail.com>
2026-06-13 18:20:49 +0800

    feat: add dynamic export to Quiz2_14 for improved rendering behavior

--- src/app/quiz2_14/page.tsx
+++ src/app/quiz2_14/page.tsx
@@ -2,6 +2,8 @@ import FeaturedBlogs_14 from './_components/home/FeaturedBlogs_14';
 import { Suspense } from 'react';
 import LoadingContainer from './_components/global/LoadingContainer';
 
+export const dynamic = 'force-dynamic';
+
 export default function Quiz2_14() {
   return (
     <>
